ef2fbb56056e365963bc41ee1394c78f8a1fdf2a
vtddggg <mxf164419@alibaba-inc.com>
2023-04-01 13:37:08 +0800

    update to 2023

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import Bio from './components/Bio.vue'
+import NewsList from './components/NewsList.vue'
+import EducationList from './components/EducationList.vue'
+import PublicationList from './components/PublicationList.vue'
+import AwardList from './components/AwardList.vue'
+import SoftwareList from './components/SoftwareList.vue'
+import TalkList from './components/TalkList.vue'
+
+</script>
+
+<template>
+  <header>
+    <Bio />
+    <hr>
+  </header>
+
+  <main>
+    <NewsList />
+    <hr>
+    <EducationList />
+    <PublicationList />
+    <AwardList />
+    <TalkList />
+    <SoftwareList />
+  </main>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/Bio.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, computed } from 'vue'
+import { convertMarkdownToHtml } from '../utils'
+import metaJson from '../content/meta.json'
+
+const metaContent = ref(metaJson)
+const description = computed(() => {
+  return convertMarkdownToHtml(metaContent.value.description)
+})
+</script>
+
+<template>
+  <div class="row items-align-center">
+    <div class="col-f4">
+      <h1>{{ metaContent.name }}</h1>
+      <div v-html="description"></div>
+    </div>
+    <div>
+      <img class="col" src="../assets/personal-photo.jpg" width="120" alt="personal photo">
+    </div>
+  </div>
+  <div class="row flex-start">
+    <div class="m-r1 m-tb-05" v-for="(contact, key) in metaContent.contact" :key="key">
+      <i :class="contact.icon"></i>
+      <a :href="contact.url" target="_blank">{{ key }}</a>
+    </div>
+  </div>
+</template>
